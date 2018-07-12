1a42607618b381e622106b9517aa9ca826b49633
Mohamad Atiye <mohamad.atiye@gmail.com>
2018-07-12 14:36:44 +0300

    no message

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
+const MAINCSS = path.join(__dirname, 'main.css');
 
 //serve the index.html
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
+  // .use((req, res) => res.sendFile(INDEX))
+  .get('/main.css', (req, res) => res.sendFile(MAINCSS))
+  .get('/', (req, res) => res.sendFile(INDEX))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
