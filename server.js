44389848e6d828a6f8a91610eeba5a6aeaebbd66
Mohamad Atiye <mohamad.atiye@gmail.com>
2018-07-12 14:07:35 +0300

    no message

--- server.js
+++ server.js
@@ -12,6 +12,40 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+
+//for sqlite db
+var sqlite3 = require('sqlite3').verbose()
+
+function registerChat(from,to,content){
+      // open the database
+      let db = new sqlite3.Database('./chatLog.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+          console.error(err.message);
+          return;
+        }
+            /////INSERT INTO TABLE
+        db.run("INSERT INTO chatLogs (cl_time,cl_from,cl_to,cl_content) VALUES (?,?,?,?)",
+            [Date.now(),from,to,content]);
+
+        db.close((err) => {
+          if (err) {
+            console.error(err.message);
+          }
+          //console.log('Close the database connection.');
+        });
+
+        console.log("chatlog: ", from,to,content);
+      });
+}
+
+
+
+
+
+
+
+
+
 //create socket server
 const io = socketIO(server);
 
@@ -22,10 +56,15 @@ io.on('connection', (socket) => {
   console.log('Client connected');
 
   socket.on('disconnect', () => {
-    console.log('Client disconnected');
-    if(!socket.nickname)return;
+    
+    if(!socket.nickname){
+      console.log('Client disconnected');
+      return;
+    }
 
     io.sockets.emit("userLeft",socket.nickname);
+    console.log("userLeft ",socket.nickname);
+
     delete users[socket.nickname];
     updateNicknames();
     
@@ -43,61 +82,36 @@ io.on('connection', (socket) => {
       users[socket.nickname] = socket;
       updateNicknames();
       io.sockets.emit("newUserJoined",data);
+      console.log("newUserJoined ",data);
     }
   });
 
   socket.on('sendMessage',(data,callback) => {
-    console.log("got message");
     data.msg = data.msg.trim();
     if(data.uid == socket.nickname && data.msg.length>0 && data.msg.length<201){
       callback(true);
 
+      var sender = data.uid;
       //is whisper?
       if(data.target in users){
         data.uid = "(whisper) "+data.uid;
         users[data.target].emit('sendMessage',data);
-        console.log("whisper to "+data.uid+" with msg: "+data.msg);
       }
       //else normal message
       else{
         socket.broadcast.emit('sendMessage',data);
+        data.target = null;
       }
 
-      // if(data.msg.charAt(0) === '@'){
-      //     //data.msg = data.msg.substring(3).trim();
-      //     var ind = data.msg.indexOf(' ');
-      //     //check we have whiper name and message
-      //     if(ind!=-1){
-      //       var name = data.msg.substring(1,ind);
-      //       var msg = data.msg.substring(ind+1);
-            
-      //       //if target user exit
-      //       if(name in users){
-      //         data.uid = "(w) "+data.uid;
-      //         data.msg = msg;
-      //         users[name].emit('sendMessage',data);
-      //         console.log("whisper to "+name+" with msg: "+msg);
-      //       }
-      //       else{
-      //         callback(false);
-      //       }
-      //     }
-      //     //whisper without message
-      //     else{
-      //       callback(false);
-      //     }
-      // }
-      // //else normal message
-      // else{
-      //   socket.broadcast.emit('sendMessage',data);
-      // }
+
+      registerChat(sender,data.target,data.msg);
+
     }
     else {
       console.log('fake user');
     }
   });
 });
-
 
 function updateNicknames(){
   io.sockets.emit("usernames",Object.keys(users));
---
+++ testdb.js
@@ -0,0 +1,76 @@
+var sqlite3 = require('sqlite3').verbose()
+//var db = new sqlite3.Database(':memory:')
+
+
+// let db = new sqlite3.Database(':memory:', (err) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     console.log('Connected to the in-memory SQlite database.');
+// });
+// db.close((err) => {
+//     if (err) {
+//         return console.error(err.message);
+//     }
+//     console.log('Close the database connection.');
+// });
+
+
+
+// db.serialize(function () {
+//   db.run('CREATE TABLE lorem (info TEXT)')
+//   var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+
+//   for (var i = 0; i < 10; i++) {
+//     stmt.run('Ipsum ' + i)
+//   }
+
+//   stmt.finalize()
+
+//   db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+//     console.log(row.id + ': ' + row.info)
+//   })
+// })
+
+// db.close()
+
+
+// open the database
+let db = new sqlite3.Database('./chatLog.db', (err) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    console.log('Connected to the chinook database.');
+
+
+    /////CREATE TABLE
+    db.run("CREATE TABLE `chatLogs` (`cl_id`	INTEGER PRIMARY KEY AUTOINCREMENT,`cl_time`	INTEGER,`cl_from` TEXT,`cl_to`	TEXT,`cl_content` TEXT)");
+
+    /////INSERT INTO TABLE
+    // db.run("INSERT INTO chatLogs (cl_time,cl_from,cl_to,cl_content) VALUES (?,?,?,?)",
+    //     [Date.now(),"mahdi",null,"someMessage text"]);
+});
+   
+
+
+
+// CREATE TABLE `testLog` (
+// 	`cl_id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+// 	`cl_time`	INTEGER,
+// 	`cl_from`	TEXT,
+// 	`cl_to`	TEXT,
+// 	`cl_content`	TEXT
+// );
+
+
+
+   
+
+//finish and close
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
