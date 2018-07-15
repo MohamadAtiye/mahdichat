c23e4b629115c43c9b1cef0c2260313b5fb0d312
DESKTOP-8PL7DR1\moham <mohamad.atiye@gmail.com>
2018-07-15 16:44:46 +0300

    added stuff

--- server.js
+++ server.js
@@ -7,18 +7,23 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 const MAINCSS = path.join(__dirname, 'main.css');
+const jqueryjs = path.join(__dirname, 'jquery.min.js');
 
-//serve the index.html
+  /////////////////////
+  //-- http server --//
+  /////////////////////
 const server = express()
   // .use((req, res) => res.sendFile(INDEX))
   .get('/main.css', (req, res) => res.sendFile(MAINCSS))
+  .get('/jquery.js', (req, res) => res.sendFile(jqueryjs))
   .get('/', (req, res) => res.sendFile(INDEX))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
-//for sqlite db
+  ////////////////////
+  //-- sqlite3 db --//
+  ////////////////////
 var sqlite3 = require('sqlite3').verbose()
-
 function registerChat(from,to,content){
       // open the database
       let db = new sqlite3.Database('./chatLog.db', sqlite3.OPEN_READWRITE, (err) => {
@@ -49,7 +54,8 @@ function getLastChat(uid, callback){
       return;
     }
 
-    let sql = 'Select * from chatLogs WHERE cl_to = "'+ uid +'" OR cl_from = "'+ uid +'"  OR cl_to IS NULL  ORDER BY cl_time DESC limit 20;'
+    // let sql = 'Select * from chatLogs WHERE cl_to = "'+ uid +'" OR cl_from = "'+ uid +'"  OR cl_to IS NULL  ORDER BY cl_time DESC limit 20;'
+    let sql = 'Select * from chatLogs WHERE cl_to IS NULL  ORDER BY cl_time DESC limit 20;'
     db.all(sql, [], (err, rows) => {
       if (err) {
         throw err;
@@ -79,60 +85,80 @@ function getLastChat(uid, callback){
 const io = socketIO(server);
 
 var users = {};
+var reservedNames = {
+  "midoking":"kingmido"
+}
 
 //handle socket events
 io.on('connection', (socket) => {
-  console.log('Client connected');
+  console.log( socket.id +' Client connected');
 
   socket.on('disconnect', () => {
     
     if(!socket.nickname){
-      console.log('Client disconnected');
+      console.log( socket.id +' Client disconnected');
       return;
     }
 
     io.sockets.emit("userLeft",socket.nickname);
-    console.log("userLeft ",socket.nickname);
-
-    delete users[socket.nickname];
-    updateNicknames();
+    console.log(socket.id +", userLeft " +socket.nickname);
     
+    delete users[socket.nickname];
+    
+    updateNicknames();
+    return;
   });
 
   socket.on('userjoin', (data,callback)=>{
-    if( data in users || data.length < 5 || data.length > 20)
+    data = sanitizeHTML(data);
+
+    if(data.length < 5 || data.length > 20)
     {
-      callback(false);
+      //callback(false);
+      callback({"res":false,"msg":"wrong name size, should be between 5 and 20 caharacters"});
+      return;
+    }
+    else if( data in users){
+      callback({"res":false,"msg":"Name already taken, please use another one"});
+      return;
+    }
+    else if(data in reservedNames){
+      callback({"res":false,"msg":"This name is reserved for special members"});
       return;
     }
     else{
+
+      if(data == "midokingmahdi123"){
+        data = "midoking";
+      }
+
       socket.nickname = data;
       users[socket.nickname] = socket;
 
       getLastChat(data,function(oldChat){
-        socket.emit('oldChat',oldChat);
+        callback({"res":true,"uid":data,"msg":"REGISTERED","payload":oldChat});
+        updateNicknames();
       });
 
-      callback(true);
-      updateNicknames();
       socket.broadcast.emit("newUserJoined",data);
       console.log("newUserJoined ",data);
     }
   });
 
-  socket.on('sendMessage',(data,callback) => {
-    data.msg = data.msg.trim();
+  socket.on('messageSent',(data,callback) => {
+    data.msg = sanitizeHTML(data.msg);
+
     if(data.uid == socket.nickname && data.msg.length>0 && data.msg.length<201){
       callback(true);
 
       var sender = data.uid;
       //is whisper?
       if(data.target in users){
-        users[data.target].emit('sendMessage',data);
+        users[data.target].emit('messageReceived',data);
       }
       //else normal message
       else{
-        socket.broadcast.emit('sendMessage',data);
+        socket.broadcast.emit('messageReceived',data);
         data.target = null;
       }
       registerChat(sender,data.target,data.msg);
@@ -147,6 +173,14 @@ io.on('connection', (socket) => {
 
 function updateNicknames(){
   io.sockets.emit("usernames",Object.keys(users));
+}
+function sanitizeHTML(input){
+  input = input.trim();
+  input =  input.replace("<","&lt;");
+  input =  input.replace(">","&gt;");
+  input =  input.replace('"',"&quot;");
+  input =  input.replace("'","&#039;");
+  return input;
 }
 
 
